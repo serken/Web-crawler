93552a83f52b56509403abd317fdd1df16b08a9d
Pavel Kharchenko <pkharchenko@productengine.com>
2014-12-08 12:16:29 +0200

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebCrawler::Application.config.secret_key_base = '1b24bdcc44cf6bf959e0dafe8564779ad74dd93ff14b717bda030d617598f231e98ccaf33b52e03a7420bc9e8a30aa9b313708e996988e1b55a21aeeac1e58b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebCrawler::Application.config.session_store :cookie_store, key: '_Web-crawler_session'
